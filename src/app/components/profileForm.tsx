b4981212eddfa7133850868b622ca65829fa7f95
ihuryferreira <ihuryferreira@icloud.com>
2025-05-09 06:53:56 -0300

    feat: implementa toast customizado com PrimeReact

--- src/app/components/profileForm.tsx
+++ src/app/components/profileForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -25,6 +25,8 @@ import {
 
 import { adicionarVenda, obterDadosLocalmente } from "@/app/services/formService";
 import { Loader2Icon } from "lucide-react";
+import { Toast } from "primereact/toast";
+import "primeicons/primeicons.css";
 
 // Definição do esquema com valores válidos
 const formSchema = z.object({
@@ -41,6 +43,7 @@ const formSchema = z.object({
 
 export function ProfileForm() {
   const [loading, setLoading] = useState(false);
+  const toast = useRef<Toast>(null);
 
   useEffect(() => {
     obterDadosLocalmente();
@@ -66,7 +69,38 @@ export function ProfileForm() {
         });
 
         console.log("Dados atualizados:", dadosAtualizados);
-        alert("Dados salvos com sucesso!");
+
+        // Exibe a notificação de sucesso
+        toast.current?.show({
+          severity: "success",
+          summary: "Mensagem de sucesso",
+          detail: "Dados gravados com sucesso",
+          life: 3000,
+          content: (
+            <div className="flex items-center justify-between gap-2 fixed bottom-4 left-1/2 transform -translate-x-1/2 w-[90%] bg-[rgb(232,250,242)] border-l-8 border-green-500 text-green-700 font-bold py-2 px-4 rounded shadow-lg">
+              <div className="flex items-center gap-2">
+                <i className="pi pi-check text-green-600 text-xl"></i>
+                <span className="text-sm">Dados gravados com sucesso</span>
+              </div>
+              <Button
+                className="bg-transparent text-destructive hover:bg-[hsl(261,87%,9%)] font-bold py-1 px-2 rounded"
+                onClick={() => toast.current?.clear()}
+              >
+                <i className="pi pi-times text-xl"></i>
+              </Button>
+            </div>
+          ),
+        });
+
+        // Reseta os valores do formulário
+        form.reset({
+          username: "",
+          sale: undefined,
+          percentage: undefined,
+        });
+
+        // Reseta manualmente o valor do Select
+        form.setValue("percentage", undefined);
       } catch (error) {
         if (error instanceof Error) {
           console.error("Erro ao salvar:", error.message);
@@ -75,98 +109,97 @@ export function ProfileForm() {
           console.error("Erro ao salvar:", error);
           alert("Erro inesperado ao salvar os dados.");
         }
+      } finally {
+        setLoading(false);
       }
-
-      // Reseta os valores do formulário
-      form.reset({
-        username: "",
-        sale: undefined,
-        percentage: undefined,
-      });
-
-      // Reseta manualmente o valor do Select
-      form.setValue("percentage", undefined);
-
-      setLoading(false);
     }, 5000);
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 py-2 w-[90%] m-auto"
-      >
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Digite o seu nome</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  value={field.value ?? ""}
-                  placeholder="ex: Douglas"
-                  className="placeholder:text-base"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="sale"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Valor da Venda</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  value={field.value ?? ""}
-                  placeholder="ex: 100 ou 127.50"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="percentage"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Escolha a Porcentagem da sua venda</FormLabel>
-              <FormControl>
-                <Select onValueChange={field.onChange} value={field.value || ""}>
-                  <SelectTrigger className="w-[100%]">
-                    <SelectValue placeholder="Selecione o seguro" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="0.065">
-                      Garantia Estendida e Troca Certa 6.5%
-                    </SelectItem>
-                    <SelectItem value="0.04">
-                      Proteção Roubo e Furto 4%
-                    </SelectItem>
-                    <SelectItem value="10">
-                      Maga+ 1 venda = R$10
-                    </SelectItem>
-                    <SelectItem value="5">
-                      Cartão Magazine Luiza 1 venda = R$5
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="w-full rounded-full text-lg hover:text-[#e2e2e2]" type="submit" disabled={loading}>
-          {loading ? <Loader2Icon className="animate-spin" /> : "Gravar dados..."}
-        </Button>
-      </form>
-    </Form>
+    <>
+      {/* Componente Toast para exibir notificações */}
+      <Toast ref={toast} />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 py-2 w-[90%] m-auto"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Digite o seu nome</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                    placeholder="ex: Douglas"
+                    className="placeholder:text-base"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="sale"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Valor da Venda</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                    placeholder="ex: 100 ou 127.50"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="percentage"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Escolha a Porcentagem da sua venda</FormLabel>
+                <FormControl>
+                  <Select onValueChange={field.onChange} value={field.value || ""}>
+                    <SelectTrigger className="w-[100%]">
+                      <SelectValue placeholder="Selecione o seguro" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="0.065">
+                        Garantia Estendida e Troca Certa 6.5%
+                      </SelectItem>
+                      <SelectItem value="0.04">
+                        Proteção Roubo e Furto 4%
+                      </SelectItem>
+                      <SelectItem value="10">
+                        Maga+ 1 venda = R$10
+                      </SelectItem>
+                      <SelectItem value="5">
+                        Cartão Magazine Luiza 1 venda = R$5
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="w-full rounded-full text-lg hover:text-[#e2e2e2]"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? <Loader2Icon className="animate-spin" /> : "Gravar dados..."}
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 }
